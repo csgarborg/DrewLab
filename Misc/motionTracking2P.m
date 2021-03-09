b361989285870cd6ed34d3243daa675ff23ac92d
csg178 <csg178@E5-ESM-10967.AD.PSU.EDU>
2021-03-09 14:39:39 -0500

    added new ftl calibration data

--- Misc/motionTracking2P.m
+++ Misc/motionTracking2P.m
@@ -357,7 +357,7 @@ if size(ballData,2) > 1
     procBallData = ballDataOnly;
 else
     procBallData = [ballData(ballDataIndex,1) ballData(ballDataIndex,2)];
-    procEMGData = zeros(length(procBallData,1));
+    procEMGData = [ballData(ballDataIndex,1) zeros(size(procBallData,1),1)];
 end
 
 % Write data values to .mat file structure
@@ -466,4 +466,38 @@ end
 
 % Distance travelled in x is difference between two distances from midline
 micronDistTraveledY = currMicronDistFromMid - prevMicronDistFromMid;
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function procData = filterEMGData(emg,sampleRate)
+
+close all
+rawEMG = emg(:,2);
+t = emg(:,1);
+% process EMG data
+fpass = [300,(sampleRate/2)-1];   % Hz
+trialDuration_sec = t(end);   % read this variable in from your data in seconds
+analogSamplingRate = sampleRate;   % Hz - change if yours is different
+dsFs = 30;   % Hz - downsampled frequency
+analogExpectedLength = trialDuration_sec*analogSamplingRate;
+trimmedEMG = rawEMG(1:min(analogExpectedLength,length(rawEMG)));
+[z,p,k] = butter(3,fpass/(analogSamplingRate/2));
+[sos,g] = zp2sos(z,p,k);
+filtEMG = filtfilt(sos,g,trimmedEMG - mean(trimmedEMG));
+kernelWidth = 0.5;
+smoothingKernel = gausswin(kernelWidth*analogSamplingRate)/sum(gausswin(kernelWidth*analogSamplingRate));
+EMGPwr = log10(conv(filtEMG.^2,smoothingKernel,'same'));
+resampEMG = resample(EMGPwr,dsFs,analogSamplingRate);
+procEMG = resampEMG;   % save this as your final array
+procT = 0:1/dsFs:t(end);
+procT = procT(1:end-1);
+semilogy(procT,procEMG)
+title('Select start and stop time (t) to use to determine mean baseline')
+tVals = ginput(2);
+close
+i = tVals(1,1) <= procT & procT <= tVals(2,1);
+baseline = mean(procEMG(i));
+procEMG = procEMG - (baseline - 1);
+procData = [procT',procEMG];
 end
